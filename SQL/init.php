cf3ccd7352842f4d7d60524010aa40b30dcf1675
angel <angeltamgho87@gmail.com>
2025-02-07 13:18:34 -0500

    update file and folder V2 update

--- SQL/init.php
+++ SQL/init.php
@@ -55,8 +55,13 @@
     // Coordonnées de l'administrateur;
     require_once ("secrets/admin_user.php");
     $adminUser = new UtilisateurDAO($BDProjet2->cBD);
-    $adminUser->ajouterUtilisateur(
-        ADMIN_NAME, ADMIN_SURNAME, ADMIN_EMAIL, ADMIN_MOTDEPASSE, ADMIN_PHONE, ADMIN_PHONE, ADMIN_PHONE, true
-    );
+    if(!$adminUser->emailExiste(ADMIN_EMAIL)){
+        $adminUser->ajouterUtilisateur(
+            ADMIN_NAME, ADMIN_SURNAME, ADMIN_EMAIL, ADMIN_MOTDEPASSE, ADMIN_PHONE, ADMIN_PHONE, ADMIN_PHONE, true
+        );
+        afficheMessageConsole("Ajout de l'administrateur avec success !");
+    }else{
+        afficheMessageConsole("Administrateur à déjà été ajouté !");
+    }
 
 
