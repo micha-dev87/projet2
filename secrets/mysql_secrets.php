d705c9d9b36b932c6dac7af68886cd72a8f2b611
angel <angeltamgho87@gmail.com>
2025-02-08 11:34:44 -0500

    update file and folder V4 update

--- secrets/mysql_secrets.php
+++ secrets/mysql_secrets.php
@@ -1,9 +1,18 @@
 <?php
-
+    /*
+    |----------------------------------------------------------------------------------|
+    | Données sensibles,
+    | Pour le developpement dans votre serveur local
+    | Definissez les valeurs DB_HOST, DB_USERNAME, DB_PASSWORD
+    | Dans votre systeme (terminal)
+    | Sur linux : export DB_HOST = ""...
+    | Sur Windows : set DB_HOST = ""...
+    |----------------------------------------------------------------------------------|
+    */
     if (getParametre("SERVER_NAME", "SERVER") == "localhost") {
-        $HOSTNAME = "localhost";
-        $USERNAME = "root";
-        $PASSWORD = "Marias1987@";
+        $HOSTNAME = getenv("DB_HOST");
+        $USERNAME = getenv("DB_USERNAME");
+        $PASSWORD = getenv("DB_PASSWORD");
     } else {
 
         $HOSTNAME = "sql210.infinityfree.com";
@@ -11,3 +20,4 @@
         $PASSWORD = "3JZXLMjuqlSw";
     }
     $PORT = 3306;
+
