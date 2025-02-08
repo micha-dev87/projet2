ec87db3f0cbcca9e9d229ae3d9804c15ddb674c8
angel <angeltamgho87@gmail.com>
2025-02-08 16:05:14 -0500

    update file and folder V4 update

--- shared/en-tete.php
+++ shared/en-tete.php
@@ -5,7 +5,7 @@
 <head>
     <title><?php echo $strTitreApplication; ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="<?php echo $strNomFichierCSS; ?>"/>
+    <link rel="stylesheet" type="text/css" href="../<?=$strNomFichierCSS?>"/>
     <!--    bootstrap integration-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome pour les icônes -->
